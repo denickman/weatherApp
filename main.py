d3947eef70bbd0464222c56391b404dc2ba94fb5
Dennis Yaremenko <yaremdennis@gmail.com>
2026-06-25 16:46:18 +0300

    add url endpoint for yearly and all data

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ Flask видит /home
 stations = pd.read_csv('data_small/stations.txt', skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ']]
 
-
 @app.route('/')      # ← ДЕКОРАТОР
 def home_page():     # ← ФУНКЦИЯ
     return render_template('home.html', data=stations.to_html()) # home.html обязательно должна быть в папке templates
@@ -67,6 +66,39 @@ def about_page(station, date):
     # }
 
     return str(temperature)  #render_template('about.html')
+
+
+
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records')
+    return result
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
