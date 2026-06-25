0120099956519797ad8c2aec1f69dd83c5ce4064
Dennis Yaremenko <yaremdennis@gmail.com>
2026-06-25 11:12:45 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+'''
+Папка templates — обязательна
+Да, это стандартное условие Flask.
+Flask ищет HTML файлы по умолчанию в папке templates/:
+'''
 
 
-# Press the green button in the gutter to run the script.
+'''
+# Flask('Website') = создаёшь новое приложение Flask
+
+Результат: объект app который может:
+- Слушать на порту
+- Обрабатывать запросы
+- Маршрутизировать URL
+'''
+app = Flask(__name__)
+
+
+'''
+Когда кто-то откроет:
+ @app.route('/home') - "слушай URL /home"
+ 
+http://localhost:5000/home
+        ↓
+Flask видит /home
+        ↓
+Смотрит: "Есть ли @app.route('/home')?"
+        ↓
+Находит эту функцию: def home()
+        ↓
+Вызывает функцию
+'''
+
+@app.route('/')          # ← ДЕКОРАТОР
+def home_page():     # ← ФУНКЦИЯ
+    return render_template('home.html') # обязательно должна быть в папке templates
+
+
+@app.route('/api/v1/<station>/<date>')
+def about_page(station, date):
+    # df = pandas.read_csv('data/' + station + '/' + date + '.csv')
+    # temperature = df.station(date)
+
+    temperature = 23
+    # return str(temperature)  #render_template('about.html')
+    return {
+        'station': station,
+        'date': date,
+        'temperature': temperature
+    }
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
